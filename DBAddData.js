8071b74985d57985247866c3db7bcdd885c232e0
IIalexandrII <sasha.shunkov@mail.ru>
2023-07-21 03:42:44 +0800

    DB

--- DBAddData.js
+++ DBAddData.js
@@ -1,3 +1,12 @@
+Array.prototype.includeObjSite = function(obj){
+   for(let element of this){
+      if(element.site_URL==obj.site){
+         return true;
+      }
+   }
+   return false;
+}
+
 import mysql from "mysql2"
 import {Model} from './dataFromAPI.js'
 
@@ -14,21 +23,35 @@ const conn = mysql.createConnection({
 }).promise();
 
 async function addIdProjInDB(conn){
-   let sqlInsertData = "INSERT INTO project_id(site_URL, id_yandex, id_topvisor, name) VALUES (?,?,?,?)";
-   let sqlSelectData = "SELECT site_URL FROM project_id";
+   let sqlInsertQuery = "INSERT INTO project_id(site_URL, id_yandex, id_topvisor, name) VALUES (?,?,?,?)";
+   let sqlSelectQuery = "SELECT site_URL FROM project_id";
    let data = await getData.getIDs();
    
-   let projFromDB; 
-   await conn.execute(sqlSelectData).then(([rows,field])=>{projFromDB = rows});
-   for(let proj of projFromDB){
-      
+   let projFromDB;
+
+   let errorCheck = false;
+   await conn.execute(sqlSelectQuery).then(([rows,field])=>{projFromDB = rows}).catch(err=>{console.log('\x1b[31m%s\x1b[0m',"error add id",err); errorCheck=true});
+   if(errorCheck) return;
+
+   let dataForAddInDB = new Array(); 
+   for(let projAPI of data){
+      if(!projFromDB.includeObjSite(projAPI)){
+         dataForAddInDB.push([projAPI.site, projAPI.idYandex, projAPI.idTopVisor, projAPI.name]);
+      }
+   }
+   if(dataForAddInDB.length>0){
+      for(let sqlData of dataForAddInDB){
+         await conn.execute(sqlInsertQuery,sqlData).then(([result,field])=>console.log('\x1b[33m%s\x1b[0m',"id added")).catch(err=>console.log('\x1b[31m%s\x1b[0m',"error add id",err));
+      }
+   }else{
+      console.log('\x1b[33m%s\x1b[0m',"no new projects found");
    }
 }
 
 async function main(){
-   await conn.connect((err)=>{if(err)console.log(err);else console.log("database connected")});
+   await conn.connect().then(result=>console.log('\x1b[32m%s\x1b[0m',"Database connected")).catch(err=>console.log('\x1b[31m%s\x1b[0m',err));
    await addIdProjInDB(conn);
-   await conn.end((err)=>{if(err)console.log(err);else console.log("database disconnected")})
+   await conn.end().then(result=>console.log('\x1b[32m%s\x1b[0m',"Database disconnected")).catch(err=>console.log('\x1b[31m%s\x1b[0m',err))
 }
 main();
 
